5a3f90c1cbcadb7f2a88b3bdef7ce2e08ad2b568
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-11-18 23:48:58 +0530

    App jsx file updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@
 import './App.css'
 
 function App() {
+  // console.log(process.env.REACT_APP_APPWRITE_URL)
+  // Above is the process to access environment variables using create-react-app. https://create-react-app.dev/docs/adding-custom-environment-variables 
+  
+  // Here, we are using vite so the process will be completely different. 
+  // https://vitejs.dev/guide/env-and-mode.html
 
+  console.log(import.meta.env.VITE_APPWRITE_URL)
 
   return (
     <>
