8fff40f0781a83a662b0188fc9203f99205eeec4
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-11-21 00:04:03 +0530

    auth js file added

---
+++ src/appwrite/auth.js
@@ -0,0 +1,26 @@
+import conf from "../conf/conf"
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({email, password, name}){
+    try{
+      const userAccount = await this.account.create(ID.unique(), email, password, name);
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService
