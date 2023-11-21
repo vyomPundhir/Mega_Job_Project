8d6145711c87282393c2cdce2bebdfa474b053f3
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-11-21 21:58:16 +0530

    auth js file updated.

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -15,10 +15,45 @@ export class AuthService {
   async createAccount({email, password, name}){
     try{
       const userAccount = await this.account.create(ID.unique(), email, password, name);
+      if (userAccount) {
+        // call another method
+        return this.login({email, password});
+      } else {
+        return userAccount;
+      }
     } catch (error) {
-      throw error
+      throw error;
     }
   }
+
+  async login({email, password}){
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+        return await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+
+    return null;
+  }
+
+  async logout() {
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+
+    return null;
+  }
+
 }
 
 const authService = new AuthService();
