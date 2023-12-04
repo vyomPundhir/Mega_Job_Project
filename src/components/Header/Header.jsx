5eb4dda46f70760e21405e91e5434ed43904d430
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-04 23:57:24 +0530

    Header jsx file updated.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,41 @@
 import React from 'react'
+import {Container, Logo, LogoutBtn} from "../index"
+import {Link} from "react-router-dom"
+import {useSelector} from "react-redux"
+import {useNavigate} from "react-router-dom"
 
 const Header = () => {
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const navItems = [
+    {
+      name: "Home",
+      slug: "/",
+      active: true,
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+    },
+    {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+    },
+    {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+    },
+    {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+    },
+  ]
+
   return (
     <div>Header</div>
   )
