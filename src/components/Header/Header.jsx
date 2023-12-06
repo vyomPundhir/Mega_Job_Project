fc5bfe818778283bcfd7928df37ed23b3a3aff10
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-06 23:26:28 +0530

    Header jsx file updated.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,7 +47,16 @@ const Header = () => {
           </div>
 
           <ul className="flex ml-auto">
-            {navItems.map((item))}
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                  onClick={() => navigate(item.slug)}
+                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
           </ul>
         </nav>
       </Container>
