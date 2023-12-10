730485145976e2970748143a7732747dfea93369
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-10 23:41:18 +0530

    PostCard jsx file added.

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import appwriteService from "../appwrite/config"
+import { Link } from 'react-router-dom'
+
+const PostCard = () => {
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
