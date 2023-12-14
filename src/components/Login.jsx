4b37df1791b2603081dceeaad11d8056ae765505
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-14 23:35:12 +0530

    Login jsx file updated.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,14 +18,37 @@ const Login = () => {
       const session = await authService.login(data)
       if (session) {
         const userData = await authService.getCurrentUser()
-        if (userData) dispatch(authLogin(userData))
+        if (userData) dispatch(authLogin(userData));
+        navigate("/")
       }
     } catch (error) {
       setError(error.message)
     }
   }
   return (
-    <div>Login</div>
+    <div
+    className='flex items-center justify-center w-full'
+    >
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px">
+            <Logo width="100%" />
+          </span>
+        </div>
+
+        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+
+        <p className="mt-2 text-center text-base text-black/60">
+          Don&apos;t have any account?&nbsp;
+          <Link
+            to="/signup"
+            className='font-medium text-primary transition-all duration-200 hover:underline'
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
