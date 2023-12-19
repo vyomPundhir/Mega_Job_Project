d4233e668c6164a592cbc933bade1a62e1b31045
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-19 23:18:27 +0530

    Signup jsx file updated.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,23 @@ import { useDispatch } from 'react-redux'
 import {useForm} from "react-hook-form"
 
 function Signup() {
+  const navigate = useNavigate()
+  const [error, setError] = useState("")
+  const dispatch = useDispatch()
+  const {register, handleSubmit} = useForm()
+
+  const create = async(data) => {
+    setError("")
+    try {
+      const userData = await authService.createAccount(data)
+      if(userData){
+        const userData = await authService.getCurrentUser()
+      }
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div>Signup</div>
   )
