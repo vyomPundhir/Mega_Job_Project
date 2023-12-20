0dea881bd35c3143a51ba8f832e4e375e218ed17
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-20 23:50:03 +0530

    Signup jsx file updated.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -18,6 +18,8 @@ function Signup() {
       const userData = await authService.createAccount(data)
       if(userData){
         const userData = await authService.getCurrentUser()
+        if(userData) dispatch(login(userData));
+        navigate("/")
       }
     } catch (error) {
       setError(error.message)
@@ -25,7 +27,27 @@ function Signup() {
   }
 
   return (
-    <div>Signup</div>
+    <div className='flex items-center justify-center'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+
+        <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+          <Link
+            to="/signup"
+            className='font-medium text-primary transition-all duration-200 hover:underline'
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
