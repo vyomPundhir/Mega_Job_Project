c115e31a0ab3d9f7e54e668d157d24eeaa4df14e
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-28 18:11:08 +0530

    AuthLAyout jsx file updated.

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -10,6 +10,15 @@ export default function Protected ({children, authentication = true})
 
   useEffect(() => {
     //TODO: make it more easy
+
+    // if(authStatus===true){
+    //   navigate("/")
+    // } else if (authStatus === false) {
+    //   navigate("/login")
+    // }
+
+    // let authValue = authStatus === true ? true : false
+
     if(authentication && authStatus !== authentication){
       navigate("/login")
     }
@@ -19,7 +28,5 @@ export default function Protected ({children, authentication = true})
     setLoader(false)
   }, [authStatus, navigate, authentication])
   
-  return (
-    <div>Protected</div>
-  )
+  return loader ? <h1>Loading...</h1> : <>{children}</>
 }
