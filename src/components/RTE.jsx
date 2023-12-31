0c7fcd4bd6e3e96e9f5559e5461ed2f9839cc5ac
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-12-31 23:36:19 +0530

    RTE jsx file updated.

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -6,6 +6,29 @@ function RTE({name, control, label, defaultValue = ""}) {
   return (
     <div className="w-full">
       {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        render={({field: {onChange}}) => (
+          <Editor 
+          initialValue={defaultValue}
+          init={{
+            height: 500,
+            menubar: false,
+            plugins: [
+            'advlist autolink lists link image  charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar:
+            'undo redo | formatselect | bold italic underline | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help'
+          }}
+          />
+        )}
+      />
+
     </div>
   )
 }
