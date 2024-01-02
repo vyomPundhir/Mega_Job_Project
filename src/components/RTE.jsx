e03a73b7bfedaacabefc713399d60c8ef437be9d
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-02 19:00:40 +0530

    RTE jsx file updated.

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -14,17 +14,35 @@ function RTE({name, control, label, defaultValue = ""}) {
           initialValue={defaultValue}
           init={{
             height: 500,
-            menubar: false,
+            menubar: true,
             plugins: [
-            'advlist autolink lists link image  charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-            ],
+            "image",
+            "advlist", 
+            "autolink", 
+            "lists", 
+            "link", 
+            "image", 
+            "charmap", 
+            "preview" 
+            ,"anchor", 
+            "searchreplace", 
+            "visualblocks", 
+            "code", 
+            "fullscreen",
+            "insertdatetime", 
+            "media", 
+            "table", 
+            "code",
+            "help",
+            "wordcount", 
+            "anchor"
+            ], 
             toolbar:
-            'undo redo | formatselect | bold italic underline | \
-            alignleft aligncenter alignright alignjustify | \
-            bullist numlist outdent indent | removeformat | help'
+            "undo redo | blocks | image |  bold italic forecolor | \
+            alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+            content_style:"body { font-family: Helvetica,Arial,sans-serif; font-size:14px }"
           }}
+          onEditorChange={onChange}
           />
         )}
       />
