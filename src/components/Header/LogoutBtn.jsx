3d6665825669fb5542503823dba4601e91991d4a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-07 23:53:42 +0530

    Logout Btn jsx file updated.

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import authService from "../../appwrite/config"
+import authService from "../../appwrite/auth"
 import {logout} from "../../store/authSlice"
 
 const LogoutBtn = () => {
@@ -12,7 +12,8 @@ const LogoutBtn = () => {
   }
 
   return (
-    <button className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"></button>
+    <button
+    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}></button>
   )
 }
 
