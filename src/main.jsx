c85204c22af06e0439a1331124eb6da14a24463a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-07 23:53:16 +0530

    main jsx file updated.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,81 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux"
 import store from "./store/store.js"
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { Login, AuthLayout } from './components/index.js'
+
+import AddPost from "./pages/AddPost.jsx"
+import Signup from "./pages/Signup.jsx"
+import EditPost from "./pages/EditPost.jsx"
+import Post from "./pages/Post.jsx"
+import AllPosts from "./pages/AllPosts.jsx"
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/login',
+        element: (
+          <AuthLayout authentication = {false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthLayout authentication = {false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/all-posts',
+        element: (
+          <AuthLayout authentication = {true}>
+            <AllPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/add-post',
+        element: (
+          <AuthLayout authentication = {true}>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/edit-post/:slug',
+        element: (
+          <AuthLayout authentication = {true}>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: '/post/:slug',
+        element: <Post />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}>
+
+      </RouterProvider>
     </Provider>
   </React.StrictMode>,
 )
