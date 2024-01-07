556c795ab41e1faed559a9368f05d920ba23eb9d
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-07 23:53:26 +0530

    index js file updated.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import Select from "./Select"
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
 import Container from "./container/Container"
@@ -6,9 +7,10 @@ import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Signup";
-import Post from "./Post"
 import PostForm from "./post-form/PostForm"
 import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
+import Input from "./Input"
 
 export {
   Header,
@@ -19,7 +21,9 @@ export {
   RTE,
   Signup,
   Login,
-  Post,
   PostForm,
-  PostCard
+  PostCard,
+  AuthLayout,
+  Select,
+  Input
 }
