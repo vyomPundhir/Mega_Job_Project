edf7267d32eaaa6bd12a73aaa0c7a39b49ffe77d
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-10 23:56:34 +0530

    LogoutBtn.jsx file updated.

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -13,7 +13,7 @@ const LogoutBtn = () => {
 
   return (
     <button
-    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}></button>
+    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full" onClick={logoutHandler}>Logout</button>
   )
 }
 
