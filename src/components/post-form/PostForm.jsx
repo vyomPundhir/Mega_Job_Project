00eac400546738b0f435e4420460d8e82ba7b9c0
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-11 00:07:39 +0530

    Post Form jsx file updated.

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,6 @@
 import React, {useCallback} from 'react'
 import { useForm } from 'react-hook-form'
-import {Button, Input, Select, RTE} from "../index"
+import {Button, Input, Select, RTE} from ".."
 import appwriteService from "../../appwrite/config"
 import { useNavigate } from 'react-router-dom' 
 import { useSelector } from 'react-redux' 
@@ -8,19 +8,19 @@ import { useSelector } from 'react-redux'
 function PostForm({post}) {
   const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
     defaultValues: {
-      title: post?.title || '',
-      slug: post?.slug || '',
-      content: post?.content || '',
+      title: post?.title || "",
+      slug: post?.$id || "",
+      content: post?.content || "",
       status: post?.status || 'active',
     },
   })
 
   const navigate = useNavigate()
-  const userData = useSelector(state => state.user.userData)
+  const userData = useSelector((state) => state.user.userData)
 
   const submit = async (data) => {
     if (post) {
-      const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+      const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
 
       if (file){
         appwriteService.deleteFile(post.featuredImage)
@@ -28,7 +28,8 @@ function PostForm({post}) {
       const dbPost = await appwriteService.updatePost(post.$id, {
         ...data,
         featuredImage: file ? file.$id : undefined,
-      })
+      });
+
       if (dbPost) {
         navigate(`/post/${dbPost.$id}`)
       }
@@ -61,17 +62,17 @@ function PostForm({post}) {
       return value
       .trim()
       .toLowerCase()
-      .replace(/^[a-zA-Z\d\s]+/g, '-')
-      .replace(/\s/g, '-')
+      .replace(/[^a-zA-Z\d\s]+/g, "-")
+      .replace(/\s/g, "-")
     
-      return ''
+      return "";
 
   }, [])
 
   React.useEffect(() => {
     const subscription = watch((value, {name}) => {
       if (name === 'title'){
-        setValue('slug', slugTransform(value.title, {shouldValidate: true}))
+        setValue('slug', slugTransform(value.title), {shouldValidate: true})
       }
     })
 
