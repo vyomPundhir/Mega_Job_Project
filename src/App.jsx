5b7914acd316a2849c8aca4542e5b851c1f41449
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-16 23:40:23 +0530

    App jsx file updated.

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,10 @@ function App() {
   }, [])
   
   return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='min-h-screen flex flex-wrap content-between bg-[#212121]'>
       <div className='w-full block'>
         <Header />
-        <main>
+        <main className='text-[white]'>
         TODO:  <Outlet />
         </main>
         <Footer />
