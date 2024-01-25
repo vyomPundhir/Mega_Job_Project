d1b8d7480dc5be61aa729bfdb2ab61989bf72beb
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-25 23:40:12 +0530

    Signup jsx file updated

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -27,26 +27,26 @@ function Signup() {
     }
 
   return (
-    <div className="flex items-center justify-center">
-            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+    <div className="flex items-center justify-center mt-[-12px]">
+            <div className={`mx-auto w-full max-w-lg bg-gradient-to-b from-[#101010] to-[#171717] rounded-xl p-10 border-[2px] border-[#3e3e4b] `}>
             <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
                 <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
-                <p className="mt-2 text-center text-base text-black/60">
+                <p className="mt-2 text-center text-base text-[#5b5b6e]">
                     Already have an account?&nbsp;
                     <Link
                         to="/login"
-                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                        className="font-medium text-primary transition-all duration-200 hover:underline text-[#808095]"
                     >
                         Sign In
                     </Link>
                 </p>
                 {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
-                <form onSubmit={handleSubmit(create)}>
+                <form onSubmit={handleSubmit(create)} className='mt-8'>
                     <div className='space-y-5'>
                         <Input
                         label="Full Name: "
@@ -74,7 +74,7 @@ function Signup() {
                         {...register("password", {
                             required: true,})}
                         />
-                        <Button type="submit" className="w-full">
+                        <Button type="submit" className="w-full text-[black] font-[600]">
                             Create Account
                         </Button>
                     </div>
