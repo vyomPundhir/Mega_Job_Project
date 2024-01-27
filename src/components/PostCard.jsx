91b56867a8e109be1b97feaf6720120039d8a871
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-01-27 23:36:55 +0530

    PostCard jsx file updated.

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
+import Tilt from 'react-parallax-tilt';
 import { Link } from 'react-router-dom'
 
 const PostCard = ({$id, title, featuredImage}) => {
   return (
-    <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4">
+    <Tilt>
+      <Link to={`/post/${$id}`}>
+      <div className="w-full bg-gradient-to-b from-[#101010] to-[#171717] border-[2px] border-[#3e3e4b] rounded-xl p-4">
         <div className="w-full justify-center mb-4">
           <img src={appwriteService.getFilePreview(featuredImage)} alt={title} 
           className='rounded-xl '/>
         </div>
         <h2 className='text-xl font-bold'>{title}</h2>
       </div>
-    </Link>
+      </Link>
+    </Tilt>
+    
   )
 }
 
